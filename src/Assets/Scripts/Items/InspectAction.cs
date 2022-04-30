9d6f7a903bb2548c77d4ac99e549f4ee16f3d3a9
Johnathon Slightham <slighthamj@gmail.com>
2022-04-30 19:06:25 -0400

    Scene 7

--- src/Assets/Scripts/Items/InspectAction.cs
+++ src/Assets/Scripts/Items/InspectAction.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class InspectAction : Interactable
 {
+    public DialogueTrigger dialogueTrigger;
 
     protected override void Action ()
     {
-        
+        Debug.Log("Inspecting");
+        dialogueTrigger.TriggerDialogue();
     }
 }
