f51f79b1d95ff620906154ea4752daac8f1238eb
Benn McGregor <benn@MacBook-Pro-9.local>
2022-04-30 19:10:06 -0400

    Added cutscene changes

--- src/Assets/Scripts/UI/SlideShow/ConditionalSlideShow.cs
+++ src/Assets/Scripts/UI/SlideShow/ConditionalSlideShow.cs
@@ -7,15 +7,18 @@ using System;
 public class ConditionalSlideShow : SlideShowBase
 {
     [SerializeField] private GameObject[] _children;
-    [SerializeField] private float _totalTime;
+    [SerializeField] private float[] _totalTimes;
+    [SerializeField] private string[] _dialogueNodeNames;
+    [SerializeField] private DialogueRunner _dialogueRunner;
 
     private float _nextActionTime = 0;
     private bool _hasStarted = false;
 
     public override void StartSlideshow(int startingSlideIdx = 0)
     {
-        _nextActionTime = Time.time + _totalTime;
+        _nextActionTime = Time.time + _totalTimes[startingSlideIdx];
         _children[startingSlideIdx].gameObject.SetActive(true);
+        _dialogueRunner.StartDialogue(_dialogueNodeNames[startingSlideIdx]);
         _hasStarted = true;
     }
 
