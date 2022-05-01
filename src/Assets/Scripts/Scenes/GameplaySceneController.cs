698f79b62d5477071019f0227f468f3ac27d3fb8
Johnathon Slightham <slighthamj@gmail.com>
2022-05-01 02:16:56 -0400

    Scene Loading

--- src/Assets/Scripts/Scenes/GameplaySceneController.cs
+++ src/Assets/Scripts/Scenes/GameplaySceneController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
+
 
 public class GameplaySceneController : MonoBehaviour
 {
@@ -8,20 +10,22 @@ public class GameplaySceneController : MonoBehaviour
     [SerializeField] private bool _hasBranchingInNextScene = false;
     private SceneLoader _sceneLoader;
 
-    private void Awake()
-    {
-        _sceneLoader = FindObjectOfType<SceneLoader>();
-    }
-
     private void Start()
     {
+        Debug.Log("Here");
         // play audio
         _audioSource.Play();
+
+        _sceneLoader = FindObjectOfType<SceneLoader>();
         
         // start asynchronously loading the next scene
         if (_sceneLoader != null && !_hasBranchingInNextScene)
         {
             _sceneLoader.LoadNextScene();
+        }
+        else 
+        {
+            Debug.Log("SceneLoader not found");
         }
     }
 
@@ -30,9 +34,10 @@ public class GameplaySceneController : MonoBehaviour
         _sceneLoader.AddToNextSceneIndex(numberOfScenesToSkip);
         _sceneLoader.LoadNextScene();
     }
-
+    [YarnCommand("go_to_next_scene")]
     public void GoToNextScene()
     {
+        _sceneLoader = FindObjectOfType<SceneLoader>();
         _sceneLoader.ActivateNextScene();
     }
 }
